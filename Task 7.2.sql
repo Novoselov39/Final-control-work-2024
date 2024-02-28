9950b6b6bd6cb3a39bef56e409a5c5896d9df3d6
Anatolii <novoselov39@gmail.com>
2024-02-28 22:00:40 +0500

    Task 7.2

---
+++ Task 7.2.sql
@@ -0,0 +1,172 @@
+CREATE database IF NOT EXISTS Human_friends1;
+USE Human_friends1;
+CREATE table IF NOT EXISTS animal_classes
+(
+	Id INT AUTO_INCREMENT PRIMARY KEY, 
+	Class_name VARCHAR(20)
+);
+
+
+
+INSERT INTO animal_classes (Class_name)
+VALUES ('Pack animals'),
+('Pets');  
+
+
+CREATE table IF NOT EXISTS packed_animals 
+(
+	  Id INT AUTO_INCREMENT PRIMARY KEY,
+    Genus_name VARCHAR (20),
+    Class_id INT,
+    FOREIGN KEY (Class_id) REFERENCES animal_classes (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO packed_animals (Genus_name, Class_id)
+VALUES ('horse', 1),
+('donkey', 1),  
+('Camel', 1); 
+
+CREATE table IF NOT EXISTS home_animals
+(
+	  Id INT AUTO_INCREMENT PRIMARY KEY,
+    Genus_name VARCHAR (20),
+    Class_id INT,
+    FOREIGN KEY (Class_id) REFERENCES animal_classes (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO home_animals (Genus_name, Class_id)
+VALUES ('cat', 2),
+('dog', 2),  
+('hamster', 2); 
+
+CREATE TABLE IF NOT exists cats
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES home_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO cats (Name, Birthday, Commands, Genus_id)
+VALUES ('Masha', '2021-01-01', 'кс-кс-кс', 1),
+('Shany', '2016-01-01', "отставить!", 1),  
+('Sergey', '2019-01-01', "", 1); 
+
+CREATE table IF NOT EXISTS dogs
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES home_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO dogs (Name, Birthday, Commands, Genus_id)
+VALUES ('Tyzik', '2024-01-01', 'ко мне, лежать, лапу, голос', 2),
+('Sharik', '2023-06-12', "сидеть, лежать, лапу", 2),  
+('boss', '2013-05-01', "сидеть, лежать, лапу, след, фас", 2), 
+('Fedor', '2022-05-10', "сидеть, лежать, фу, место", 2);
+
+CREATE table IF NOT EXISTS hamsters
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES home_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO hamsters (Name, Birthday, Commands, Genus_id)
+VALUES ('Garri', '2023-10-12', '', 3),
+('Ron', '2022-03-12', "атака сверху", 3),  
+('Cherlok', '2024-07-11', NULL, 3), 
+('Vatson', '2024-05-10', NULL, 3);
+
+CREATE table IF NOT EXISTS horses
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES packed_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO horses (Name, Birthday, Commands, Genus_id)
+VALUES ('Grom', '2029-01-12', 'бегом, шагом', 1),
+('Byran', '2015-03-12', "бегом, шагом, хоп", 1),  
+('Angel', '2016-07-12', "бегом, шагом, хоп, брр", 1), 
+('Vylkan', '2023-11-10', "бегом, шагом, хоп", 1);
+
+CREATE table IF NOT EXISTS donkeys 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES packed_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO donkeys (Name, Birthday, Commands, Genus_id)
+VALUES ('Liz', '2019-04-10', NULL, 2),
+('Gerl', '2020-03-12', "", 2),  
+('Klara', '2021-07-12', "", 2), 
+('Perl', '2022-12-10', NULL, 2);
+
+CREATE table IF NOT EXISTS camels 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES packed_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO camels (Name, Birthday, Commands, Genus_id)
+VALUES ('Lila', '2022-04-10', 'вернись', 3),
+('Bo', '2019-03-12', "остановись", 3),  
+('Omlet', '2015-07-12', "повернись", 3), 
+('Hloya', '2022-12-10', "улыбнись", 3);
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM camels;
+
+SELECT Name, Birthday, Commands FROM horses
+UNION SELECT  Name, Birthday, Commands FROM donkeys;
+CREATE TEMPORARY table IF NOT EXISTS animals AS 
+SELECT *, 'Лошади' as genus FROM horses
+UNION SELECT *, 'Ослы' AS genus FROM donkeys
+UNION SELECT *, 'Собаки' AS genus FROM dogs
+UNION SELECT *, 'Кошки' AS genus FROM cats
+UNION SELECT *, 'Хомяки' AS genus FROM hamsters;
+
+CREATE table IF NOT EXISTS yang_animal AS
+SELECT Name, Birthday, Commands, genus, TIMESTAMPDIFF(MONTH, Birthday, CURDATE()) AS Age_in_month
+FROM animals WHERE Birthday BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR) AND ADDDATE(CURDATE(), INTERVAL -1 YEAR);
+
+USE Human_friends1;
+
+SELECT * FROM yang_animal;
+SELECT h.Name, h.Birthday, h.Commands, pa.Genus_name, ya.Age_in_month 
+FROM horses h
+LEFT JOIN yang_animal ya ON ya.Name = h.Name
+LEFT JOIN packed_animals pa ON pa.Id = h.Genus_id
+UNION 
+SELECT d.Name, d.Birthday, d.Commands, pa.Genus_name, ya.Age_in_month 
+FROM donkeys d 
+LEFT JOIN yang_animal ya ON ya.Name = d.Name
+LEFT JOIN packed_animals pa ON pa.Id = d.Genus_id
+UNION
+SELECT c.Name, c.Birthday, c.Commands, ha.Genus_name, ya.Age_in_month 
+FROM cats c
+LEFT JOIN yang_animal ya ON ya.Name = c.Name
+LEFT JOIN home_animals ha ON ha.Id = c.Genus_id
+UNION
+SELECT d.Name, d.Birthday, d.Commands, ha.Genus_name, ya.Age_in_month 
+FROM dogs d
+LEFT JOIN yang_animal ya ON ya.Name = d.Name
+LEFT JOIN home_animals ha ON ha.Id = d.Genus_id
+UNION
+SELECT hm.Name, hm.Birthday, hm.Commands, ha.Genus_name, ya.Age_in_month 
+FROM hamsters hm
+LEFT JOIN yang_animal ya ON ya.Name = hm.Name
+LEFT JOIN home_animals ha ON ha.Id = hm.Genus_id;
